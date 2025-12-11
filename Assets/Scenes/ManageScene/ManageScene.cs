3790022006b4871416545a097375a6d1e7073b4a
mikalaromero <mikalaromero4933@gmail.com>
2025-12-11 12:54:31 -0500

    final project

--- Assets/Scenes/ManageScene/ManageScene.cs
+++ Assets/Scenes/ManageScene/ManageScene.cs
@@ -1,35 +1,21 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
 public class ManageScene : MonoBehaviour
 
 {
 
- BackgroundAudio audioManager;
+    BackgroundAudio audioManager;
   [SerializeField]
   private string sceneToLoad;
     private void OnMouseDown()
     {
-       Button btn = GetComponent<Button>();
-    if (btn != null && !btn.interactable)
-        return;  // Don’t load if locked
-
-    SceneManager.LoadScene(sceneToLoad);
-    audioManager.PlaySFX(audioManager.click);
+        SceneManager.LoadScene(sceneToLoad);
+        audioManager.PlaySFX(audioManager.click);
     }
-     public Button[] buttons;
     private void Awake()
     {
-        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            buttons[i].interactable = false; 
-        }
-        for (int i = 0; i <unlockedLevel; i++)
-        {
-           buttons[i].interactable = true; 
-        }
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<BackgroundAudio>();
     }
 }
